61b3ce743de35e6937f2505c6f377fff79b809db
Logan Demmy <frank.demmy@gmail.com>
2021-04-05 14:59:39 -0500

    nav bar paths

---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const AboutMe = () => (
+    <>
+        <div className="statement">
+            <section className="personal">
+                <h2>Future Web Dev</h2>
+                <p>I am currently enrolled in a C# focused web development bootcamp hosted by Nashville Software School</p>
+            </section>
+            {/* <picture className="photo">
+                <img className="headshot" src="/images/Logan .jpg" alt="Headshot of Logan"> </img>
+            </picture> */}
+        </div>
+    </>
+)
---
+++ src/components/Journal.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { NavBar } from "./nav/NavBar.js"
+import { Application } from "./Application"
+
+export const Journal = () => ( 
+    <>
+        <NavBar />
+        <Application />
+    </>
+)
