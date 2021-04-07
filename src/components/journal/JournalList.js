54caa12619ff09648ee7bf0f2ba8a51c7f8c6011
Logan Demmy <frank.demmy@gmail.com>
2021-04-07 10:55:48 -0500

    Delete Button for Posts

--- src/components/journal/JournalList.js
+++ src/components/journal/JournalList.js
@@ -1,15 +1,22 @@
 import  React, { useState, useEffect } from 'react'
 import { EntryCard } from './EntryCard'
-import { getJournalEntries } from '../../modules/EntryManager'
+import { getJournalEntries, deletePost } from '../../modules/EntryManager'
+import { useParams } from 'react-router';
 
 export const EntryList = () => {
-    const [entries, setEntries] = useState([])
+    const [entries, setEntries] = useState([]);
+
 
     const getEntries = () => {
         return getJournalEntries().then(entriesFromAPI => {
             setEntries(entriesFromAPI)
         });
     };
+
+    const deleteSetPost = (id) => {
+        deletePost(id)
+        .then(() => getJournalEntries().then(setEntries))
+    }
 
     useEffect(() => {
         getEntries();
@@ -18,7 +25,8 @@ export const EntryList = () => {
     return(
         <div className="entry-cards">
             {entries.map(entry => <EntryCard entry={entry}
-                                            key={entry.id}/>)}
+                                            key={entry.id}
+                                            deletePost={deleteSetPost}/>)}
         </div>
     )
 };
