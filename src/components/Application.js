5589bd837bd4fa46d53454985a9263ce568f260d
Logan Demmy <frank.demmy@gmail.com>
2021-04-07 10:35:13 -0500

    entries loading from heroku

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { AboutMe } from "./AboutMe"
+import { EntryList } from "./journal/JournalList"
 
 export const Application = () => {
     return (
@@ -8,6 +9,10 @@ export const Application = () => {
             
             <Route exact path="/">
                 <AboutMe />
+            </Route>
+
+            <Route path="/journal">
+                <EntryList />
             </Route>
             
         </>
---
+++ src/modules/EntryManager.js
@@ -0,0 +1,57 @@
+//Comment out depending on local or hosted testing
+//const apiUrl = "http://localhost:8088"
+const apiUrl = "https://firstjsonserver.herokuapp.com"
+
+let orderedResponse = [];
+export const getJournalEntries = () => {
+
+    return fetch(`${apiUrl}/entries?_expand=user`)
+    .then(response => response.json())
+    .then(parsedResponse => {
+        orderedResponse = parsedResponse.reverse();
+        console.log(orderedResponse);
+        return orderedResponse;
+    })
+}
+
+export const createPost = postObj => {
+    return fetch(`${apiUrl}/entries`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+        
+    })
+        .then(response => response.json())
+        .then(getJournalEntries())
+        
+}
+
+export const deletePost = (postId) => {
+    return fetch(`${apiUrl}/entries/${postId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => response.json())
+        .then(getJournalEntries())
+}
+
+export const getSingleEntry = (postId) => {
+    return fetch(`${apiUrl}/entries/${postId}`)
+        .then(response => response.json())
+}
+
+export const updateEntry = (postObj) => {
+    return fetch(`${apiUrl}/${postObj.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+    })
+        .then(response => response.json())
+        .then(getJournalEntries)
+}
