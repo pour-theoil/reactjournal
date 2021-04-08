72b4b0b86ede6d28e98399c0c1952e873c22d5bb
Logan Demmy <frank.demmy@gmail.com>
2021-04-07 20:22:23 -0500

    add image and styling

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -2,11 +2,13 @@ import React from "react"
 import { NavBar } from "./nav/NavBar.js"
 import { Application } from "./Application"
 import "./Journal.css"
+import { Footer } from './Footer'
 
 export const Journal = () => ( 
     <>
         <h1 className="header">Daily Journal</h1>
         <NavBar />
         <Application />
+        <Footer />
     </>
 )
